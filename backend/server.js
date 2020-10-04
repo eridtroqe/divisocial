88ef2ae01ba32b48ff7e8208086a61de4d8ab02d
EridTroqe <etroqe@ikons-see.com>
2020-10-05 00:06:31 +0200

    added BE functionalities

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const http = require('http');
-
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -42,12 +43,8 @@ app.use((req, res, next) => {
 });
 
 
-
-
-
-
-
-
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 
 
